4b82e82b685a3414a16ce329a08dc1707d089b15
prekira <prekira@hotmail.com>
2018-01-31 16:16:51 -0800

    html

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
+var plants = [
+//emty to start
+//object for each plant
+{
+name: "palm tree"
+light: "low"
+water: "high"
+},
+{
+name: "cactus"
+},
+//put between bracket 
+//variables only inside obj
+];
 
+
+size
