e691f14be6ead12bc5d466ca8230a3fcc108cdc4
Yukati <yukati.g@gmail.com>
2018-03-21 16:23:23 -0700

    radio buttons

--- catalog.js
+++ catalog.js
@@ -1,33 +1,29 @@
+
+
 var plants = [
-//emty to start
-//object for each plant
-{
-  name: "palm tree",
-  light: "high",
-  water: "low"
-},
-{
-  name: "letizia succulent",
-  light: "medium",
-  water: "low"
-},
-{
-  name:"california succulent",
-  light:,
-  water:
-}
-{
-  name: "parva succulent",
-  light: ,
-  water:
-}
-{
-  name: "cherry blossom tree",
-  light: ,
-  water:
-}
-{
-  name: ""
+  { light: "low", name: "cactus" },
+  { light: "high", name: "flower" }
+];
+
+var light = document.getElementById("light");
+var radio = document.getElementById("radio");
+var button = document.getElementById("button");
+
+
+function searchItems() {
+  var inputValue = light.value;
+
+  for (let i = 0; i < plants.length; i++) {
+    var plant = plants[i];
+    if (plant.light === inputValue) {
+      console.log(plant);
+      var plantDiv = document.createElement("div");
+
+      var content = document.createTextNode(plant.name);
+      plantDiv.appendChild(content);
+      document.body.appendChild(plantDiv);
+    }
+  }
 }
 //put between bracket
 //variables only inside obj
